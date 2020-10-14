5467029b2ad451cc38f64eac352e53ac89c6fc08
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-10-13 22:25:48 -0500

    create Pokedex array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-alert('Hello world!');
-let favoriteFood = 'Ice Cream 🍨';
-document.write(favoriteFood);
+let pokemonList = [];
+
+pokemonList[0] = {
+  name: 'Bulbasaur',
+  height: 0.7,
+  types: ['grass','poison']
+};
+
+pokemonList[1] = {
+  name: 'Shroomish',
+  height: 0.4,
+  types: 'grass'
+};
+
+pokemonList[2] = {
+  name: 'Archeops',
+  height: 1.4,
+  types: ['rock', 'flying']
+};
