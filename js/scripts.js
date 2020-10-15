518fd1ed2f0de9511e3372e8a22aaaff138f9e3a
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-10-14 20:44:56 -0500

    add big pokemon condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
+// initialize an empty array
 let pokemonList = [];
 
+//populate the array with pokemons
 pokemonList[0] = {
   name: 'Bulbasaur',
   height: 0.7,
@@ -18,6 +20,12 @@ pokemonList[2] = {
   types: ['rock', 'flying']
 };
 
+//make for loop to get names and heights of each pokemon and print them
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})<br>`);
+  document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`);
+  //add special flare if pokemon is bigger than 1 meter
+  if (pokemonList[i].height > 1) {
+    document.write(' - Wow, that\'s a big pokemon!');
+  }
+  document.write('<br>');
 }
