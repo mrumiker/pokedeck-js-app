61559aa6d021b9fd295ef0adcb06ab09db4ab0b2
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-10-30 14:57:27 -0500

    create Bootstrap eventlistener
    Finally got it!

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,7 @@ let pokemonRepository = (function() {
     return 0;
   }
   //Create function to make buttons with name of each pokemon
+  //Create function to make buttons with name of each pokemon
   function addListItem(pokemon) {
     let pokeList = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
@@ -25,55 +26,40 @@ let pokemonRepository = (function() {
     button.setAttribute('type', 'button');
     button.setAttribute('data-toggle', 'modal');
     button.setAttribute('data-target', '#pokeModal');
+    button.setAttribute('data-pokemonobject', JSON.stringify(pokemon));
     listItem.appendChild(button);
     pokeList.appendChild(listItem);
     //log pokemon info in modal when button is clicked
-    button.addEventListener('click', function() {
-      //clear modal before loading new data
-      $('.modal').removeData('bs.modal');
-      //get pokemon stats and send to modal
-      showDetails(pokemon);
-    });
-  }
-  //create function to show details of pokemon in modal
-  function showDetails(pokemon) {
-    //Set variables in modal
-    let titleElement = document.querySelector('#pokename');
-    let heightElement = document.querySelector('#height');
-    let weightElement = document.querySelector('#weight');
-    let imageElement = document.querySelector('#pokepic');
-    //Create loading screen for slow connections
-    titleElement.innerText = 'Loading...';
-    heightElement.innerText = '...';
-    weightElement.innerText = '...';
-    imageElement.src = '#';
+    $('#pokeModal').on('show.bs.modal', function(event) {
+      var button = $(event.relatedTarget);
+      var pokemon = button.data('pokemonobject');
+      var modal = $(this);
+      loadDetails(pokemon).then(function() {
+        modal.find('#pokename').text(pokemon.name);
+        if (pokemon.height > 49) {
+          modal.find('#height').text(`height:  ${pokemon.height} - Wow, that's a big pokemon! 🤩`);
+        }
+        else {
+          modal.find('#height').text(`height:  ${pokemon.height}`);
+        }
 
-    loadDetails(pokemon).then(function() {
+        if (pokemon.weight > 499) {
+          modal.find('#weight').text(`weight:  ${pokemon.weight} - Wow, that's a plump pokemon! 🤩`);
+        }
+        else {
+          modal.find('#weight').text(`weight:  ${pokemon.weight}`);
+        }
 
-      titleElement.innerText = pokemon.name;
+        modal.find('#pokepic').attr('src', pokemon.imageUrl);
+        modal.find('#pokepic').attr('alt', `picture of ${pokemon.name}`);
 
-      if (pokemon.height > 49) {
-        heightElement.innerText = (`height:  ${pokemon.height} - Wow, that's a big pokemon! 🤩`);
-      }
-      else {
-        heightElement.innerText = (`height:  ${pokemon.height}`);
-      }
+      }).catch(function(e) {
+        /* eslint-disable no-console */
+        console.error(e);
+        /* eslint-enable no-console */
+      });
+    })
 
-      if (pokemon.weight > 499) {
-        weightElement.innerText = (`weight:  ${pokemon.weight} - Wow, that's a plump pokemon! 🤩`);
-      }
-      else {
-        weightElement.innerText = (`weight:  ${pokemon.weight}`);
-      }
-      
-      imageElement.src = pokemon.imageUrl;
-      imageElement.setAttribute('alt', `picture of ${pokemon.name}`);
-
-    }).catch(function(e) {
-      /* eslint-disable no-console */
-      console.error(e);
-      /* eslint-enable no-console */
-    });
   }
 
   //create function to get pokemons from external api
@@ -105,7 +91,6 @@ let pokemonRepository = (function() {
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.weight = details.weight;
-      item.types = details.types;
     }).catch(function(e) {
       /* eslint-disable no-console */
       console.error(e);
